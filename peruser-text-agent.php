07629989c21f0656305021179cbaae17107409d3
Ahmed Hosni <ahmedhosni.contact@gmail.com>
2015-08-10 22:08:19 +0000

    Add per user notification

---
+++ peruser-text-agent.php
@@ -0,0 +1,32 @@
+<?php 
+
+    $title   =  "Notification message";
+	$message =  htmlspecialchars($_POST['message']); 
+	$user    =  htmlspecialchars($_POST['user']); 
+
+//echo 'The Residence Tunis Push notification \n';
+
+	$out = shell_exec("node peruser-push.js '".$user."' '".$message."'");
+	?>
+	<script language="javascript">
+	/*
+    var ask = window.confirm(<?php echo "Broadcast message result:\n".$out; ?>);
+    if (ask) {
+        window.alert("This post was successfully deleted.");
+
+        document.location.href = "index.html";
+
+    } */
+
+	if(!confirm( "Broadcast text message has been processed.\n\n Do you want to send another message?")) {
+		document.location = 'index.html';
+	}
+	else {
+	document.location = 'index.html#contact';
+	}
+	
+	</script>
+<?php
+	//header('Location: index.html');      
+
+?>
