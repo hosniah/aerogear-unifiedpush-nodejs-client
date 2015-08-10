685fd71c896c117aad52809195362a733f8e16c8
Ahmed Hosni <ahmedhosni.contact@gmail.com>
2015-08-10 18:16:03 +0000

    Add stuff for URL pushes

---
+++ url-agent.php
@@ -0,0 +1,31 @@
+<?php 
+
+    $title   =  "Notification message";
+	$message =  htmlspecialchars($_POST['message']); 
+
+//echo 'The Residence Tunis Push notification \n';
+
+	$out = shell_exec("node push.js '".$message."'");
+	?>
+	<script language="javascript">
+	/*
+    var ask = window.confirm(<?php echo "Broadcast message result:\n".$out; ?>);
+    if (ask) {
+        window.alert("This post was successfully deleted.");
+
+        document.location.href = "index.html";
+
+    } */
+
+	if(!confirm( "Broadcast text message has been processed.\n\n Do you want to send another message?")) {
+		document.location = 'index.html';
+	}
+	else {
+	document.location = 'index.html#contact';
+	}
+	
+	</script>
+<?php
+	//header('Location: index.html');      
+
+?>
