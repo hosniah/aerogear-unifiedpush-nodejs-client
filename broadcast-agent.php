ddb5337385191dc5193310af3cadb3710f6a0e8a
Ahmed Hosni <ahmedhosni.contact@gmail.com>
2015-08-10 15:31:52 +0000

    use push contact as node params

--- broadcast-agent.php
+++ broadcast-agent.php
@@ -1,12 +1,31 @@
 <?php 
 
-	echo htmlspecialchars($_POST['title']); 
-	echo htmlspecialchars($_POST['message']); 
+    $title   =  htmlspecialchars($_POST['title']); 
+	$message =  htmlspecialchars($_POST['message']); 
 
 //echo 'The Residence Tunis Push notification \n';
 
-	$out = shell_exec("node push.js");
-	echo $out;
+	$out = shell_exec("node push.js $title $message");
+	?>
+	<script language="javascript">
+	/*
+    var ask = window.confirm(<?php echo "Broadcast message result:\n".$out; ?>);
+    if (ask) {
+        window.alert("This post was successfully deleted.");
 
+        document.location.href = "index.html";
+
+    } */
+
+	if(!confirm( "Broadcast text message has been processed.\n\n Do you want to send another message?")) {
+		document.location = 'index.html';
+	}
+	else {
+	document.location = 'index.html#contact';
+	}
+	
+	</script>
+<?php
+	//header('Location: index.html');      
 
 ?>
